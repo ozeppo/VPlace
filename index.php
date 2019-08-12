de8dac8dfd9dd844cd61665cbb01e189b5f31c24
Filip Chmielecki <filip@gry.int.pl>
2019-08-12 15:06:27 +0200

    bug fixes

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <link rel="stylesheet" type="text/css" href="css/main.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
     <meta charset="utf-8">
-    <title>TYTUŁ</title>
+    <title>VPlace - Zaloguj się</title>
   </head>
   <body>
 	<div class="wrapper">
