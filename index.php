2373385f93785d421566f6518a7ca3159c44eb10
Filip Chmielecki <filip@gry.int.pl>
2019-08-12 15:04:08 +0200

    Register > Main

--- index.php
+++ index.php
@@ -14,9 +14,8 @@
 				<form class="signform" action="index.php" method="post">
 					<input type="text" placeholder="E-Mail" name="login" value="">
 					<input type="password" placeholder="Hasło" name="password" value="">
-					<input type="submit" name="" id="button" value="Zaloguj">
+					<input type="submit" name="" class="button" value="Zaloguj">
 					<br>
-					<a class="rejestracja" href="subpage\register.php">Nie masz jeszcze konta?</a>
 
 				</form>
 
@@ -35,63 +34,21 @@
 
 
 		<div class="main">
-			<h2>Witaj na VPlace</h2>
+			<h2>Rejestracja</h2>
 
-<p>Lorem ipsum dolor sit amet,
- consectetur adipiscing elit. Phasellus dignissim feugiat lacus,
- non imperdiet libero porttitor eu. Maecenas a mi non ligula gravida dignissim.
- Donec varius elit mauris. Duis in porta mi.
- Sed tincidunt rhoncus leo,
- non feugiat lorem vestibulum sed. Maecenas eget dolor consectetur,
- laoreet dolor in, tincidunt libero. Duis vel mauris hendrerit, placerat ipsum eget,
- eleifend risus. Quisque efficitur elit ac tempus tincidunt. Vestibulum sed metus sagittis,
- mollis neque eu,
- mollis magna. Suspendisse potenti.</p>
+      <form class="" action="index.php" method="post">
+        <input type="email" placeholder="E-Mail" name="Login" value=""><br>
+        <input type="text" placeholder="Imię" id="name" name="name" value=""><br>
+        <input type="text" placeholder="Nazwisko" id="surname" name="" value=""><br>
+        <input type="password" placeholder="Hasło" name="password" value=""><br>
+        <input type="checkbox" name="accept" id="accept" value="">
+        Zgadzam się na <a id="ToS" href="#">Warunki Użytkowania</a><br>
+        <input type="submit" name="" class="button" value="Zarejestruj"><br>
+      </form>
 
-<p>Sed id urna ut ipsum cursus blandit.
- Nulla facilisi. Nunc nisl lorem, pulvinar sit amet nibh nec,
- gravida hendrerit nulla. Curabitur enim ante, tristique nec ante sed,
- blandit gravida eros. Fusce tincidunt ex id neque tempor, at porta nisi porta.
- Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
- Nam sagittis urna vel leo fringilla, in aliquet turpis rutrum. Pellentesque quis volutpat mauris,
- quis lobortis libero. Curabitur dictum arcu ac purus vulputate interdum. Nulla vitae ante tempor,
- ornare mi sed, porttitor purus. Nulla ac convallis turpis,
- sit amet tristique sem. Aenean non metus neque.</p>
+			<div class="filler">
 
-<p>Nam quis ligula nec risus vulputate blandit.
- Vestibulum vel ante fermentum, aliquet nisi eu,
- vehicula diam. Donec ornare nulla quam, id sagittis erat placerat eget.
- Nam pharetra ipsum massa, non molestie elit tincidunt sit amet. Ut malesuada arcu posuere tellus blandit imperdiet.
- Donec consequat sapien eget quam efficitur luctus. Proin facilisis, tellus at ornare consectetur, odio massa tempus quam,
- a bibendum nunc velit a orci. Nulla porta ante magna, non tempus odio mattis a. Donec dictum lobortis blandit. In gravida dui quis arcu lacinia,
- sit amet efficitur nulla euismod. Nam imperdiet et orci nec tristique. Suspendisse posuere efficitur lectus, vel lacinia felis fermentum a.
- Quisque eget sagittis nunc. Etiam quis tristique nulla. Curabitur mollis lorem justo, viverra sodales ipsum eleifend vel. Suspendisse sagittis,
- erat in consequat imperdiet, sem urna lobortis elit, vel semper nibh sapien a nisl.</p>
-
-<p>Fusce placerat tellus nisi,
- vel finibus felis mollis id.
- Aliquam aliquam mauris sem, ultricies feugiat dolor fringilla a.
- Fusce molestie pretium elementum. Duis sed nulla eu lectus lacinia egestas.
- Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
- Donec erat tellus, pharetra at tellus consequat, hendrerit semper risus. Quisque justo erat,
- vehicula ut tempus id, ultricies at nisi. Sed at est tortor. Nullam elementum sollicitudin nisl,
- eu dapibus nisi fringilla sit amet. Donec ut ultricies nulla, id ornare velit. Vestibulum at augue tempus,
- tempus purus id, consectetur sem. Nunc pharetra, elit et placerat aliquet, ligula nunc tristique tellus,
- non euismod enim nibh sit amet magna.</p>
-
-<p>Curabitur nec viverra orci.
- Donec elementum dignissim eros vitae consequat.
- Donec id nisi dignissim, iaculis libero varius,
- maximus ex. Etiam tristique velit ut ipsum accumsan sagittis.
- Sed scelerisque est sit amet risus bibendum, quis ornare est dapibus.
- Nulla ut mi ac dolor pellentesque facilisis. Ut pellentesque aliquam posuere.
- Nullam efficitur sed metus sed feugiat. Curabitur vel eros sed lorem sagittis vehicula vel sit amet ipsum.
- Vivamus congue dui eu vehicula rhoncus. Fusce aliquet vitae libero nec ultricies. Cras vitae velit vel lorem faucibus tincidunt.
- Phasellus porttitor congue nulla, eu posuere ante condimentum quis. </p>
-
-
-
-			<div class="filler">s</div>
+      </div>
 
 		</div>
 
